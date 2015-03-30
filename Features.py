abf02ab1e227bda5c57ac1ad81bc4eaa6e3c5461
Shusil <shusil.dangi@gmail.com>
2015-03-30 12:50:29 -0400

    Added FKI features, functions to create Image

--- Features.py
+++ Features.py
@@ -31,3 +31,45 @@ def xvar(symbol):
 def yvar(symbol):
     return [NP.var(symbol.ys())]
 
+## FKI Features 
+def getFKIfeatures(I):
+    [W,H] = I.shape
+    c4ant = H+1
+    c5ant = 0
+    f = NP.zeros((W,9)) # 
+    for x in range(W):
+        c1=0; c2=0; c3=0; c4=H+1; c5=0; c6=H+1; c7=0; c8=0; c9=0;
+        for y in range(H):
+            if(I[x][y]==1):
+                c1+=1       # number of white pixels in the column
+                c2+=y       # center of gravity of the column
+                c3+=y**2    # second order moment of the column
+                if(y<c4):
+                    c4=y    # position of the upper contour in the column
+                if(y>c5):
+                    c5=y    # position of the lower contour in the column
+            if(y>1 & I[x][y-1]!= I[x][y-2]):
+                c8+=1       # Number of black-white transitions in the column
+        
+        c2 /= H
+        c3 /= H**2
+        
+        for y in range(c4+1,c5):
+            if(I[x][y]==1):
+                c9+=1       # Number of white pixels between the upper and lower contours
+        
+        if(x+1<W):
+            for y in range(H):
+                if(I[x+1][y-1]==1):
+                    if(y<c6):
+                        c6=y     
+                    if(y>c7):
+                        c7=y
+                    
+        c6 = (c6-c4ant)/2 # Orientation of the upper contour in the column
+        c7 = (c7-c5ant)/2 # Orientation of the lower contour in the column
+        c4ant = c4
+        c5ant = c5
+        f[x] = NP.array([c1,c2,c3,c4,c5,c6,c7,c8,c9])
+    return f
+    
--- runme.py
+++ runme.py
@@ -4,48 +4,68 @@ Created on Fri Mar 27 19:26:05 2015
 
 @author: sxd7257
 """
+import SymbolData
+import Features
+import matplotlib.pyplot as plt
+import numpy as np
 
-# def normalize
+#import skimage
+from skimage.morphology import disk
+from skimage.filter import rank
+#from skimage import data, filter
+
 def interp( x12, y12, t):
     x = x12[0] * (1- t) + x12[1] * t
     y = y12[0] * (1 -t) + y12[1] * t
     return (x, y)
 
+def normalize(symbols,scale):
+    i=0
+    for symbol in symbols:
+        xmin = min(symbol.xs())
+        ymin = min(symbol.ys())
+        for i in range(len(symbol.strokes)):
+            for j in range(len(symbol.strokes[i].xs)):
+                symbol.strokes[i].xs[j] = (symbol.strokes[i].xs[j]-xmin)*scale/2
+                symbol.strokes[i].ys[j] = (symbol.strokes[i].ys[j]-ymin)*scale/2
+        symbols[i] = symbol
+        i+=1
+    return(symbols)
 
-import SymbolData
-import matplotlib.pyplot as plt
-import numpy as np
+def getImg(symbol):
+       xs = np.array([])
+       ys = np.array([])
 
+       for i in range(len(symbol.strokes)):
+           for j in range(len(symbol.strokes[i].xs)-1):
+               for t in np.linspace(0,1,30):
+                   x,y = interp(symbol.strokes[i].xs[j:j+2],symbol.strokes[i].ys[j:j+2],t)
+                   xs = np.append(xs,x)
+                   ys = np.append(ys,y)
+       I = np.zeros((max(symbol.ys())+1,max(symbol.xs())+1))
+
+       for i in range(len(xs)):
+           I[max(symbol.ys())-int(ys[i])][int(xs[i])] = 1
+
+       I = rank.mean(I, selem=disk(1))
+       
+       for i in range(I.shape[0]):
+           for j in range(I.shape[1]):
+               if(I[i][j]>0.5):
+                   I[i][j]=1
+       return I
+      
+      
 symbols = SymbolData.unpickleSymbols("train.dat")
+scale = 99
+symbols = normalize(symbols,scale)
+symbols[0].plot()
+plt.grid('on')
 
-xmin = min(symbols[0].xs())
-ymin = min(symbols[0].ys())
-
-scale = 49
-plt.figure(1)
-for i in range(len(symbols[0].strokes)):
-    for j in range(len(symbols[0].strokes[i].xs)):
-        symbols[0].strokes[i].xs[j] = (symbols[0].strokes[i].xs[j]-xmin)*scale
-        symbols[0].strokes[i].ys[j] = (symbols[0].strokes[i].ys[j]-ymin)*scale
-    plt.plot(symbols[0].strokes[i].xs,symbols[0].strokes[i].ys,'b')
-plt.show()
-
-xs = np.array([])
-ys = np.array([])
-for i in range(len(symbols[0].strokes)):
-    for j in range(len(symbols[0].strokes[i].xs)-1):
-        for t in np.linspace(0,1,30):
-            x,y = interp(symbols[0].strokes[i].xs[j:j+2],symbols[0].strokes[i].ys[j:j+2],t)
-            xs = np.append(xs,x)
-            ys = np.append(ys,y)
-#plt.show()
-
-I = np.zeros((max(symbols[0].ys())+1,max(symbols[0].xs())+1))
-
-plt.figure(2)
-#for i in range(len(symbols[0].xs())):
-#    I[max(symbols[0].ys())-int(symbols[0].ys()[i])][int(symbols[0].xs()[i])] = 1
-for i in range(len(xs)):
-    I[max(symbols[0].ys())-int(ys[i])][int(xs[i])] = 1
+I = getImg(symbols[6])
     
 plt.imshow(I)
+plt.gray()
+plt.show()
+
+f = Features.getFKIfeatures(I)
