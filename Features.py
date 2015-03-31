f2385b2df35fe4818b49a4ea4b4b85c95e5a2673
Shusil <shusil.dangi@gmail.com>
2015-03-31 13:19:47 -0400

    Added mean and variance in fki features.
    Also shuffled some functions from runme.py to other files
    added aditional normalization in SymbolData.py

--- Features.py
+++ Features.py
@@ -1,20 +1,65 @@
 import numpy as NP
-import SymbolData
-
+#import SymbolData
+from skimage.morphology import disk
+from skimage.filter import rank
+import matplotlib.pyplot as plt
 
 # This is a skeleton of a file that will contain functions for various features.
 
 def features(symbols):
-    return map ( (lambda symbol: symbolFeatures(symbol)), symbols)
+    return list(map ( (lambda symbol: symbolFeatures(symbol)), symbols))
 
+# Linear Interpolation
+def interp( x12, y12, t):
+    x = x12[0] * (1- t) + x12[1] * t
+    y = y12[0] * (1 -t) + y12[1] * t
+    return (x, y)
+
+# Create an image from a symbol
+def getImg(symbol):
+       xs = NP.array([])
+       ys = NP.array([])
+
+       for i in range(len(symbol.strokes)):
+           for j in range(len(symbol.strokes[i].xs)-1):
+               for t in NP.linspace(0,1,30):
+                   x,y = interp(symbol.strokes[i].xs[j:j+2],symbol.strokes[i].ys[j:j+2],t)
+                   xs = NP.append(xs,x)
+                   ys = NP.append(ys,y)
+       I = NP.zeros((max(symbol.ys())+1,max(symbol.xs())+1))
+
+       for i in range(len(xs)):
+           I[max(symbol.ys())-int(ys[i])][int(xs[i])] = 1
+
+       I = rank.mean(I, selem=disk(1))
+       
+       for i in range(I.shape[0]):
+           for j in range(I.shape[1]):
+               if(I[i][j]>0.5):
+                   I[i][j]=1
+       return I
+
+# Show image for the symbol
+def showImg(symbol):
+    plt.figure()
+    I = getImg(symbol)
+    plt.imshow(I)
+    plt.gray()
+    plt.show()
+    
+# Get the features from a symbol
 def symbolFeatures(symbol):
-    f = []
-
+    f = NP.array([])
+    
     #Call feature functions here like so:
-    f = f + xmean(symbol)
-    f = f + ymean(symbol)
-    f = f + xvar(symbol)
-    f = f + yvar(symbol)    
+    f = NP.append(f,xmean(symbol))
+    f = NP.append(f,ymean(symbol))
+    f = NP.append(f,xvar(symbol))
+    f = NP.append(f,yvar(symbol))
+    I = getImg(symbol)
+    fkiFeat = getFKIfeatures(I)
+    fki = getMeanStd(fkiFeat)
+    f = NP.append(f,fki)    
     return f
 
 # Some really simple global properties to start us off.
@@ -33,14 +78,14 @@ def yvar(symbol):
 
 ## FKI Features 
 def getFKIfeatures(I):
-    [W,H] = I.shape
+    [H,W] = I.shape
     c4ant = H+1
     c5ant = 0
     f = NP.zeros((W,9)) # 
     for x in range(W):
         c1=0; c2=0; c3=0; c4=H+1; c5=0; c6=H+1; c7=0; c8=0; c9=0;
         for y in range(H):
-            if(I[x][y]==1):
+            if(I[y][x]==1):
                 c1+=1       # number of white pixels in the column
                 c2+=y       # center of gravity of the column
                 c3+=y**2    # second order moment of the column
@@ -48,19 +93,19 @@ def getFKIfeatures(I):
                     c4=y    # position of the upper contour in the column
                 if(y>c5):
                     c5=y    # position of the lower contour in the column
-            if(y>1 & I[x][y-1]!= I[x][y-2]):
+            if(y>1 & I[y-1][x]!= I[y-2][x]):
                 c8+=1       # Number of black-white transitions in the column
         
         c2 /= H
         c3 /= H**2
         
         for y in range(c4+1,c5):
-            if(I[x][y]==1):
+            if(I[y][x]==1):
                 c9+=1       # Number of white pixels between the upper and lower contours
         
         if(x+1<W):
             for y in range(H):
-                if(I[x+1][y-1]==1):
+                if(I[y-1][x+1]==1):
                     if(y<c6):
                         c6=y     
                     if(y>c7):
@@ -72,4 +117,16 @@ def getFKIfeatures(I):
         c5ant = c5
         f[x] = NP.array([c1,c2,c3,c4,c5,c6,c7,c8,c9])
     return f
+
+## Get Mean and Variance of FKI features
+def getMeanStd(f):
+    mean = NP.mean(f, axis=0)
+    std = NP.std(f, axis=0)
+    feat = NP.append(mean,std)
+    return(feat)
     
+## RWTH Features
+#def getRWTHfeatures(I,w):
+#    [W,H] = I.shape
+#    for i in range(W):
+        
--- runme.py
+++ runme.py
@@ -7,65 +7,19 @@ Created on Fri Mar 27 19:26:05 2015
 import SymbolData
 import Features
 import matplotlib.pyplot as plt
-import numpy as np
 
-#import skimage
-from skimage.morphology import disk
-from skimage.filter import rank
-#from skimage import data, filter
-
-def interp( x12, y12, t):
-    x = x12[0] * (1- t) + x12[1] * t
-    y = y12[0] * (1 -t) + y12[1] * t
-    return (x, y)
-
-def normalize(symbols,scale):
-    i=0
-    for symbol in symbols:
-        xmin = min(symbol.xs())
-        ymin = min(symbol.ys())
-        for i in range(len(symbol.strokes)):
-            for j in range(len(symbol.strokes[i].xs)):
-                symbol.strokes[i].xs[j] = (symbol.strokes[i].xs[j]-xmin)*scale/2
-                symbol.strokes[i].ys[j] = (symbol.strokes[i].ys[j]-ymin)*scale/2
-        symbols[i] = symbol
-        i+=1
-    return(symbols)
-
-def getImg(symbol):
-       xs = np.array([])
-       ys = np.array([])
-
-       for i in range(len(symbol.strokes)):
-           for j in range(len(symbol.strokes[i].xs)-1):
-               for t in np.linspace(0,1,30):
-                   x,y = interp(symbol.strokes[i].xs[j:j+2],symbol.strokes[i].ys[j:j+2],t)
-                   xs = np.append(xs,x)
-                   ys = np.append(ys,y)
-       I = np.zeros((max(symbol.ys())+1,max(symbol.xs())+1))
-
-       for i in range(len(xs)):
-           I[max(symbol.ys())-int(ys[i])][int(xs[i])] = 1
-
-       I = rank.mean(I, selem=disk(1))
-       
-       for i in range(I.shape[0]):
-           for j in range(I.shape[1]):
-               if(I[i][j]>0.5):
-                   I[i][j]=1
-       return I
-      
       
 symbols = SymbolData.unpickleSymbols("train.dat")
 scale = 99
-symbols = normalize(symbols,scale)
+symbols = Features.normalize(symbols,scale)
 symbols[0].plot()
 plt.grid('on')
 
-I = getImg(symbols[6])
+I = Features.getImg(symbols[0])
     
 plt.imshow(I)
 plt.gray()
 plt.show()
 
 f = Features.getFKIfeatures(I)
+fki = Features.getMeanStd(f)
